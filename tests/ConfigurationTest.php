4e6e6c5845f6e24605ecc92d4c3fa8cf2e714ec8
Sarel van der Walt <sarel@afrihost.com>
2019-10-16 20:32:21 +0200

    Fully tested Configuration class.

--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -1,38 +1,54 @@
 <?php
 
-namespace Afrihost\SwarmProcess;
+use Afrihost\SwarmProcess\Configuration;
+use Afrihost\SwarmProcess\SwarmProcess;
 
-
-class ConfigurationTest extends \PHPUnit_Framework_TestCase
+class ConfigurationTest extends PHPUnit\Framework\TestCase
 {
+    /** @var SwarmProcess */
+    private $swarm;
 
-    public function testGetTickLoopDelayMicroseconds()
+    /** @var Configuration */
+    private $swarmConfig;
+
+    protected function setUp()
     {
-
-    }
-
-    public function testIsEnforceProcessTimeouts()
-    {
-
+        $this->swarm = new SwarmProcess();
+        $this->swarmConfig = new Configuration();
     }
 
     public function testSetEnforceProcessTimeouts()
     {
+        $this->assertFalse($this->swarmConfig->isEnforceProcessTimeouts());
 
-    }
-
-    public function testGetCompletedCallback()
-    {
-
+        $this->swarmConfig->setEnforceProcessTimeouts(true);
+        $this->assertTrue($this->swarmConfig->isEnforceProcessTimeouts());
     }
 
     public function testSetCompletedCallback()
     {
+        $this->assertNull($this->swarmConfig->getCompletedCallback());
 
+        $this->swarmConfig->setCompletedCallback(function () {
+            // do nothing, it's just a callable
+        });
+        $this->assertIsCallable($this->swarmConfig->getCompletedCallback());
+    }
+
+    public function testSetCompletedCallbackMustBeCallable()
+    {
+        $this->expectException('Exception');
+
+        $this->swarmConfig->setCompletedCallback('thisIsNotACallable');
     }
 
     public function testSetTickLoopDelayMicroseconds()
     {
+        // check default is 0:
+        $this->assertEquals(0, $this->swarmConfig->getTickLoopDelayMicroseconds());
 
+        // test set and get:
+        $this->swarmConfig->setTickLoopDelayMicroseconds(1234);
+        $this->assertEquals(1234, $this->swarmConfig->getTickLoopDelayMicroseconds());
     }
 }
--- tests/SwarmProcessTest.php
+++ tests/SwarmProcessTest.php
@@ -1,4 +1,6 @@
 <?php
+
+use Afrihost\SwarmProcess\Configuration;
 use Afrihost\SwarmProcess\SwarmProcess;
 
 /**
@@ -11,7 +13,8 @@ class SwarmProcessTest extends PHPUnit\Framework\TestCase
     /** @var SwarmProcess */
     private $swarm;
 
-    protected function setUp() {
+    protected function setUp()
+    {
         $this->swarm = new SwarmProcess();
     }
 
@@ -38,4 +41,37 @@ class SwarmProcessTest extends PHPUnit\Framework\TestCase
         $this->swarm->setMaxRunStackSize(-1);
     }
 
+    public function testConfigurationSetting()
+    {
+        $config = new Configuration();
+        // cannot set the callable, because clojure's are not serializable (and the assertion needs this)
+        $config->setEnforceProcessTimeouts(true);
+        $config->setTickLoopDelayMicroseconds(1234);
+
+        $this->swarm->setConfiguration($config);
+
+        $this->assertEquals($config, $this->swarm->getConfiguration());
+    }
+
+    public function testFunctionalFullRun()
+    {
+        $this->swarm->setMaxRunStackSize(2);
+        $this->assertEquals(2, $this->swarm->getMaxRunStackSize());
+
+        // We don't actually give it a command to run below, because the tests may be run on windows/linux.
+        $this->swarm->pushNativeCommandOnQueue('');
+        $this->assertEquals(1, $this->swarm->getStackCount());
+        $this->swarm->pushNativeCommandOnQueue('');
+        $this->assertEquals(2, $this->swarm->getStackCount());
+        $this->swarm->pushNativeCommandOnQueue('');
+        $this->assertEquals(3, $this->swarm->getStackCount());
+        $this->swarm->pushNativeCommandOnQueue('');
+        $this->assertEquals(4, $this->swarm->getStackCount());
+
+        $this->swarm->getConfiguration()->setTickLoopDelayMicroseconds(1); // code coverage of tick delay
+
+        $this->swarm->run();
+
+        $this->assertEquals(4, $this->swarm->getSuccessfulProcessCount());
+    }
 }
