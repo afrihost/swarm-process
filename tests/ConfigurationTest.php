a5f6da198e0d1c77c97a2944a45cc6dca463941b
Sarel van der Walt <sarel@afrihost.com>
2019-10-16 20:27:46 +0200

    fix phpunit.xml to work with latest phpunit

---
+++ tests/ConfigurationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Afrihost\SwarmProcess;
+
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetTickLoopDelayMicroseconds()
+    {
+
+    }
+
+    public function testIsEnforceProcessTimeouts()
+    {
+
+    }
+
+    public function testSetEnforceProcessTimeouts()
+    {
+
+    }
+
+    public function testGetCompletedCallback()
+    {
+
+    }
+
+    public function testSetCompletedCallback()
+    {
+
+    }
+
+    public function testSetTickLoopDelayMicroseconds()
+    {
+
+    }
+}
